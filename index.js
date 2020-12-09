f9c6bdd5ea44a900b3d7c9ba23e5cb2926c48196
adelazalewski <paliuadela@yahoo.com>
2020-12-09 16:17:37 -0500

    added dotenv and made the port dynamic with environment variables to be able to deploy the app to heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const posts = require("./data/db");
 const postsRouter = require("./posts/posts-router");
 const server = express();
+const port = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(postsRouter);
@@ -9,6 +11,6 @@ server.use(postsRouter);
 server.get("/", (req, res) => {
     res.send("welcome to a new api by adela zalewski!")
 })
-server.listen(4000, () => {
-    console.log("server listenig at http://localhost:4000")
+server.listen(port, () => {
+    console.log(`server listenig at http://localhost:${port}`)
 })
